dec1632a7b109dc248675bd527e67898b3ede570
Chinmaynahar <naharchinmay@gmail.com>
2026-02-20 20:42:16 +0530

    Initial Setup

---
+++ src/test/java/api/SampleTest.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testExample() {
+        System.out.println("Automation framework initialized.");
+    }
+}
